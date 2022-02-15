7c2274cd34f3512696a8525932a316d04149f8a2
Garrett Quathamer <gquathamer@gmail.com>
2022-02-14 19:40:54 -0800

    Completed javascript-primitive-and-variables - Garrett Quathamer

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,29 @@
+var fullName = "Garrett Quathamer";
+
+var isCool = false;
+
+var totalPets = 1;
+
+console.log("fullname : ", fullName);
+
+console.log("isCool: ", isCool);
+
+console.log("totalPets: ", totalPets);
+
+console.log("typeof fullName: ", typeof fullName);
+
+console.log("typeof isCool: ", typeof isCool);
+
+console.log("typeof totalPets: ", typeof totalPets);
+
+var unknown = null;
+
+console.log("Null: ", unknown);
+
+console.log("typeof unknown: ", typeof unknown);
+
+var never;
+
+console.log("never: ", never);
+
+console.log("typeof never: ", typeof never);
