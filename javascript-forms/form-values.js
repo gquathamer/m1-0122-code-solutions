88cdcff1609969f8055087245ac946bf4bef23bf
Garrett Quathamer <gquathamer@gmail.com>
2022-03-15 19:55:28 -0700

    moved the formObject object literal to the inside of the callback function on form submit event

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,9 +1,8 @@
 var contactForm = document.forms[0];
 
-var formControl = {};
-
 contactForm.addEventListener('submit', function (event) {
   event.preventDefault();
+  var formControl = {};
   for (var i = 0; i < contactForm.elements.length - 1; i++) {
     formControl[contactForm.elements[i].name] = contactForm.elements[i].value;
   }
